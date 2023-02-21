699504df75aca92904607b1e4d3c48a03453f00e
Philip Keller <philipjoelkeller@gmail.com>
2023-02-20 20:37:21 -0800

    Initial Commit

---
+++ CatchEmAll/CatchEmAllApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchEmAllApp.swift
+//  CatchEmAll
+//
+//  Created by Philip Keller on 2/20/23.
+//
+
+import SwiftUI
+
+@main
+struct CatchEmAllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
