46fdd09293c45f4d95a0eb42eea5517c5748ced7
Philip Keller <philipjoelkeller@gmail.com>
2023-02-21 13:23:25 -0800

    Circular Progress View - YouTube 6.7

--- CatchEmAll/CreaturesViewModel.swift
+++ CatchEmAll/CreaturesViewModel.swift
@@ -12,20 +12,23 @@ class CreatureViewModel: ObservableObject {
     
     private struct Returned: Codable {
         var count: Int
-        var next: String? // TODO: We'll want to change this to an option, but will demo why later
+        var next: String?
         var results: [Creature]
     }
     
     @Published var urlString = "https://pokeapi.co/api/v2/pokemon/"
     @Published var count = 0
     @Published var creaturesArray: [Creature] = []
+    @Published var isLoading = false
     
     func getData() async {
         print("🕸️ We are accessing the url \(urlString)")
+        isLoading = true
         
         // Conver urlString to a special URL type
         guard let url = URL(string: urlString) else {
             print("😡 ERROR: Could not create a URL from \(urlString)")
+            isLoading = false
             return
         }
         
@@ -35,12 +38,15 @@ class CreatureViewModel: ObservableObject {
             // Try to decode JSON data into our own data structures
             guard let returned = try? JSONDecoder().decode(Returned.self, from: data) else {
                 print("😡 ERROR: Could not decode returned JSON data")
+                isLoading = false
                 return
             }
             self.count = returned.count
             self.urlString = returned.next ?? ""
             self.creaturesArray = self.creaturesArray + returned.results
+            isLoading = false
         } catch {
+            isLoading = false
             print("😡 ERROR: Could not use URL at \(urlString) to get data and a response")
         }
     }
