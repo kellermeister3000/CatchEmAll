fd353db83b973a248394c45ca6467514fd4160a3
Philip Keller <philipjoelkeller@gmail.com>
2023-02-20 23:45:20 -0800

    App Icon and Launch with List View Dummy Data - YouTube 6.2

---
+++ CatchEmAll/CreaturesListView.swift
@@ -0,0 +1,29 @@
+//
+//  CreaturesListView.swift
+//  CatchEmAll
+//
+//  Created by Philip Keller on 2/20/23.
+//
+
+import SwiftUI
+
+struct CreaturesListView: View {
+    var creatures = ["Pikachu", "Squirtle", "Charzard", "Snorlax"]
+    
+    var body: some View {
+        NavigationStack {
+            List(creatures, id: \.self) { creature in
+                Text(creature)
+                    .font(.title2)
+            }
+            .listStyle(.plain)
+            .navigationTitle("Pokemon")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreaturesListView()
+    }
+}
