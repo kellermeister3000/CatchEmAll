1e52cbc16fbfc4e8438dd8585f87ebeb34749b34
Philip Keller <philipjoelkeller@gmail.com>
2023-02-21 21:56:25 -0800

    Refactored Image View as Extension - YouTube 6.11.1

--- CatchEmAll/DetailView.swift
+++ CatchEmAll/DetailView.swift
@@ -24,44 +24,7 @@ struct DetailView: View {
                 .padding(.bottom)
             
             HStack {
-                AsyncImage(url: URL(string: creatureDetailVM.imageURL)) { phase in
-                    if let image = phase.image { // We have a valid image
-                        let _ = print("VALID IMAGE ***")
-                        image
-                            .resizable()
-                            .scaledToFit()
-                            .background(.white)
-                            .frame(width: 96, height: 96)
-                            .cornerRadius(16)
-                            .shadow(radius: 8, x: 5, y: 5)
-                            .overlay {
-                                RoundedRectangle(cornerRadius: 16)
-                                    .stroke(.gray.opacity(0.5), lineWidth: 1)
-                            }
-                            .padding(.trailing)
-                        
-                    } else if phase.error != nil { // We have an error
-                        let _ = print("!!! ERROR LOADING IMAGE!!!")
-                        Image(systemName: "questionmark.square.dashed")
-                            .resizable()
-                            .scaledToFit()
-//                            .background(.white)
-                            .frame(width: 96, height: 96)
-                            .cornerRadius(16)
-                            .shadow(radius: 8, x: 5, y: 5)
-                            .overlay {
-                                RoundedRectangle(cornerRadius: 16)
-                                    .stroke(.gray.opacity(0.5), lineWidth: 1)
-                            }
-                            .padding(.trailing)
-                    } else { // Use a placeholder
-                        let _ = print("PPP Placeholder Image")
-                        Rectangle()
-                            .foregroundColor(.clear)
-                            .frame(width: 96, height: 96)
-                            .padding(.trailing)
-                    }
-                }
+                creatureImage
                 
                 VStack (alignment: .leading) {
                     HStack {
@@ -96,6 +59,46 @@ struct DetailView: View {
         .task {
             creatureDetailVM.urlString = creature.url
             await creatureDetailVM.getData()
+        }
+    }
+}
+
+extension DetailView {
+    var creatureImage: some View {
+        AsyncImage(url: URL(string: creatureDetailVM.imageURL)) { phase in
+            if let image = phase.image { // We have a valid image
+                image
+                    .resizable()
+                    .scaledToFit()
+                    .background(.white)
+                    .frame(width: 96, height: 96)
+                    .cornerRadius(16)
+                    .shadow(radius: 8, x: 5, y: 5)
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 16)
+                            .stroke(.gray.opacity(0.5), lineWidth: 1)
+                    }
+                    .padding(.trailing)
+                
+            } else if phase.error != nil { // We have an error
+                Image(systemName: "questionmark.square.dashed")
+                    .resizable()
+                    .scaledToFit()
+//                            .background(.white)
+                    .frame(width: 96, height: 96)
+                    .cornerRadius(16)
+                    .shadow(radius: 8, x: 5, y: 5)
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 16)
+                            .stroke(.gray.opacity(0.5), lineWidth: 1)
+                    }
+                    .padding(.trailing)
+            } else { // Use a placeholder
+                Rectangle()
+                    .foregroundColor(.clear)
+                    .frame(width: 96, height: 96)
+                    .padding(.trailing)
+            }
         }
     }
 }
